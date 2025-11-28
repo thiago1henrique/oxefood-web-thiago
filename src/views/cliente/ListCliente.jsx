49feb444eb4f0de1bdc79112df2e28464de40bd4
Thiago Henrique <1thiago1henrique@gmail.com>
2025-11-28 16:02:20 -0300

    feat: aula 17 - frontend cliente, entregador e produto

--- src/views/cliente/ListCliente.jsx
+++ src/views/cliente/ListCliente.jsx
@@ -1,12 +1,39 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+import {Button, Container, Divider, Header, Icon, Modal, Table} from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
 export default function ListCliente () {
 
     const [lista, setLista] = useState([]);
+
+    const [openModal, setOpenModal] = useState(false);
+    const [idRemover, setIdRemover] = useState();
+
+    async function remover() {
+
+        await axios.delete('http://localhost:8080/api/cliente/' + idRemover)
+            .then((response) => {
+
+                console.log('Cliente removido com sucesso.')
+
+                axios.get("http://localhost:8080/api/cliente")
+                    .then((response) => {
+                        setLista(response.data)
+                    })
+            })
+            .catch((error) => {
+                console.log('Erro ao remover um cliente.')
+            })
+        setOpenModal(false)
+    }
+
+
+    function confirmaRemover(id) {
+        setOpenModal(true)
+        setIdRemover(id)
+    }
 
     useEffect(() => {
         carregarLista();
@@ -94,9 +121,11 @@ export default function ListCliente () {
                                                 circular
                                                 color='red'
                                                 title='Clique aqui para remover este cliente'
-                                                icon>
+                                                icon
+                                                onClick={e => confirmaRemover(cliente.id)}>
                                                 <Icon name='trash' />
                                             </Button>
+
 
                                         </Table.Cell>
                                     </Table.Row>
@@ -107,6 +136,27 @@ export default function ListCliente () {
                     </div>
                 </Container>
             </div>
+
+            <Modal
+                basic
+                onClose={() => setOpenModal(false)}
+                onOpen={() => setOpenModal(true)}
+                open={openModal}
+            >
+                <Header icon>
+                    <Icon name='trash' />
+                    <div style={{marginTop: '5%'}}> Tem certeza que deseja remover esse registro? </div>
+                </Header>
+                <Modal.Actions>
+                    <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                        <Icon name='remove' /> Não
+                    </Button>
+                    <Button color='green' inverted onClick={() => remover()}>
+                        <Icon name='checkmark' /> Sim
+                    </Button>
+                </Modal.Actions>
+            </Modal>
+
 
         </div>
     )
--- src/views/entregador/ListEntregador.jsx
+++ src/views/entregador/ListEntregador.jsx
@@ -1,12 +1,39 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+import {Button, Container, Divider, Header, Icon, Modal, Table} from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
 export default function ListEntregador () {
 
     const [lista, setLista] = useState([]);
+
+    const [openModal, setOpenModal] = useState(false);
+    const [idRemover, setIdRemover] = useState();
+
+    function confirmaRemover(id) {
+        setOpenModal(true)
+        setIdRemover(id)
+    }
+
+    async function remover() {
+
+        await axios.delete('http://localhost:8080/api/entregador/' + idRemover)
+            .then((response) => {
+
+                console.log('Entregador removido com sucesso.')
+
+                axios.get("http://localhost:8080/api/entregador")
+                    .then((response) => {
+                        setLista(response.data)
+                    })
+            })
+            .catch((error) => {
+                console.log('Erro ao remover um entregador.')
+            })
+        setOpenModal(false)
+    }
+
 
     useEffect(() => {
         carregarLista();
@@ -93,6 +120,7 @@ export default function ListEntregador () {
                                                 inverted
                                                 circular
                                                 color='red'
+                                                onClick={e => confirmaRemover(entregador.id)}
                                                 title='Clique aqui para remover este entregador'
                                                 icon>
                                                 <Icon name='trash' />
@@ -107,6 +135,27 @@ export default function ListEntregador () {
                     </div>
                 </Container>
             </div>
+
+            <Modal
+                basic
+                onClose={() => setOpenModal(false)}
+                onOpen={() => setOpenModal(true)}
+                open={openModal}
+            >
+                <Header icon>
+                    <Icon name='trash' />
+                    <div style={{marginTop: '5%'}}> Tem certeza que deseja remover esse registro? </div>
+                </Header>
+                <Modal.Actions>
+                    <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                        <Icon name='remove' /> Não
+                    </Button>
+                    <Button color='green' inverted onClick={() => remover()}>
+                        <Icon name='checkmark' /> Sim
+                    </Button>
+                </Modal.Actions>
+            </Modal>
+
 
         </div>
     )
